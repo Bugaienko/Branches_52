59cd15ccbd7ded5b65cfc60b3064f4ccec9dc506
YuliiaSydorenko <o972986990f@gmail.com>
2024-10-28 15:05:28 +0100

    TestName

---
+++ src/ViktoriiaRomanenko/task_01.java
@@ -0,0 +1,8 @@
+package ViktoriiaRomanenko;
+
+public class task_01 {
+    @Override
+    public String toString() {
+        return "Viktoriia Romanenko";
+    }
+}
---
+++ src/kharina_olga/task1.java
@@ -0,0 +1,9 @@
+package kharina_olga;
+
+public class task1 {
+
+    @Override
+    public String toString() {
+        return "Olga Kharina";
+    }
+}
