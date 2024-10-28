adb704765b48099bef9fd46bf1f971cb4791c34c
SergeyB <sergiibugaienko@gmail.com>
2024-10-28 12:28:15 +0100

    crate user-repo

---
+++ src/UserRepo.java
@@ -0,0 +1,12 @@
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 28.10.2024
+ */
+
+public class UserRepo {
+
+    public Person addPerson(Person person) {
+        Person newPerson = new Person();
+        return newPerson;
+    }
+}
