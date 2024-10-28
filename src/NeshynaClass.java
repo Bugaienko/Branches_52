5c1650357414dfc09f15711c6c5500c45e542261
Neshyna <mariya.neshyna@gmail.com>
2024-10-28 12:49:49 +0100

    masha

--- src/NeshynaClass.java
+++ src/NeshynaClass.java
@@ -2,4 +2,8 @@ public class NeshynaClass {
     public static void main(String[] args) {
         System.out.println("neshyna");
     }
+
+    public String toString(){
+        return "neshyna";
+    }
 }
