5105dceccd10539166fa740837085635977c061d
SergeyB <sergiibugaienko@gmail.com>
2024-10-28 10:26:22 +0100

    init commit

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 28.10.2024
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
