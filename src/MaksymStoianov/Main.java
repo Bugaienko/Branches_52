9511efd619fb63cf6b997feb3896f8aeb16e7dba
Maksym Stoyanov <sto.maks+github.com@gmail.com>
2024-10-28 12:45:05 +0100

    add: package 'MaksymStoianov'

---
+++ src/MaksymStoianov/Main.java
@@ -0,0 +1,15 @@
+package MaksymStoianov;
+
+/**
+ * Main
+ *
+ * @author <a href="stoianov.maksym@gmail.com">Maksym Stoianov</a>
+ */
+public class Main {
+
+  @Override
+  public String toString() {
+    return "Maksym Stoianov";
+  }
+
+}
