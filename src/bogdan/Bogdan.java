737c644ca4b203dd83c4d23178674e4ed1d2d378
BogdanF <sovtour@gmail.com>
2024-10-28 12:55:14 +0100

    Class Bogdan

---
+++ src/bogdan/Bogdan.java
@@ -0,0 +1,16 @@
+package bogdan;/* Group: 52-1, "AIT Hi-tech team" GMBH 
+Author: Bogdan Fesenko
+Date: 28-10-2024
+*/
+
+/*
+
+ */
+public class Bogdan {
+
+        @Override
+        public String toString() {
+            return "Bogdan";
+        }
+    }
+
